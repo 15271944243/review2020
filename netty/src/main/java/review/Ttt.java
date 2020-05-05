ef90f8e3936a3a7444a56fafa5e1780e2828c861
xxx <190899887@qq.com>
2020-05-05 23:34:39 +0800

    netty demo

--- netty/src/main/java/review/Ttt.java
+++
@@ -1,9 +0,0 @@
-package review;
-
-/**
- * @description:
- * @author: xiaoxiaoxiang
- * @date: 2020/5/1 10:17
- */
-public class Ttt {
-}
---
+++ netty/src/main/java/review/introduction/NettyClientHandler.java
@@ -0,0 +1,38 @@
+package review.introduction;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @description:
+ * @author: xiaoxiaoxiang
+ * @date: 2020/5/5 22:21
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        String request = "the time: " + System.currentTimeMillis();
+        ByteBuf req = Unpooled.copiedBuffer(request.getBytes());
+        ctx.writeAndFlush(req);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("the Client receive from Server： " + body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
