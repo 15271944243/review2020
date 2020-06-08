5595d1c6059d689b6ae6b739386abad255874038
xxx <190899887@qq.com>
2020-06-08 23:19:42 +0800

    algorithm demo

---
+++ algorithm/src/main/java/review/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,18 @@
+package review.tree;
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * @author: xiaoxiaoxiang
+ * @date: 2020/6/8 23:10
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+    /**
+     * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+     */
+
+    /**
+     * 题目意思: 给定一个二叉树,返回他的层级顺序遍历的节点值;(从左到右,一层一层地遍历)
+     * 思路: 广度优先搜索(Breadth First Search)
+     */
+}
