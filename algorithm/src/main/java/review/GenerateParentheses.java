d29e0bced10255dfba0576ef62d8bca3c7100892
xxx <190899887@qq.com>
2020-06-16 23:37:11 +0800

    algorithm demo

---
+++ algorithm/src/main/java/review/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package review;
+
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ * @author: xiaoxiaoxiang
+ * @date: 2020/6/16 23:16
+ */
+public class GenerateParentheses {
+
+    /**
+     *  Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+     *
+     *  For example, given n = 3, a solution set is:
+     *  [
+     *     "((()))",
+     *     "(()())",
+     *     "(())()",
+     *     "()(())",
+     *     "()()()"
+     *  ]
+     */
+
+    /**
+     * 题目意思: 给定n对圆括号,编写一个函数来生成格式正确(即左括号与右括号数量一样)的圆括号的所有组合。
+     * 思路一: 使用递归进行遍历生成
+     * 思路二: 使用动态规划
+     */
+
+
+
+
+}
