e1f5f39471379e562a00b9bd02c8b2c288a3adf2
xiaoxiaoxiang <xiaoxiaoxiang@xhqb.com>
2020-08-20 09:43:59 +0800

    update readme.md

--- basic/src/main/java/review/threadlocal/ThreadLocalCase01.java
+++ basic/src/main/java/review/threadlocal/ThreadLocalCase01.java
@@ -1,30 +1,48 @@
 package review.threadlocal;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- * @description:
+ * ThreadLocal会出现内存泄漏问题
  * @author: xiaoxiaoxiang
  * @date: 2020/8/13 13:57
  */
 public class ThreadLocalCase01 {
 
-    ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        threadLocal.get();
+//        Thread t1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                threadLocal.get();
+//
+//            }
+//        }, "t1");
+//        t1.start();
+    }
 
     public void aaa() {
 
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                threadLocal.set("t1");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 threadLocal.get();
             }
         }, "t1");
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                threadLocal.set("t2");
-            }
-        }, "t2");
+//        Thread t2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                threadLocal.set("t2");
+//            }
+//        }, "t2");
         threadLocal.remove();
     }
 }
--- redis/readme.md
+++ redis/readme.md
@@ -1,2 +1,5 @@
 - redis基础用法
 - spring-redis-session原理
+
+参考下文,lettuce连接redis主从、哨兵、集群等
+https://www.cnblogs.com/throwable/p/11601538.html
