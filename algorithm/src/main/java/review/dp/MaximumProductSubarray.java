0767fe9101320e3fbf948025d52dbe597edeb475
xiaoxiaoxiang <xiaoxiaoxiang@xhqb.com>
2020-08-21 16:48:10 +0800

    add algorithm demo

---
+++ algorithm/src/main/java/review/dp/MaximumProductSubarray.java
@@ -0,0 +1,37 @@
+package review.dp;
+
+/**
+ * https://leetcode.com/problems/maximum-product-subarray/ No. 152 乘积最大子序列
+ * @author: xiaoxiaoxiang
+ * @date: 2020/8/21 16:24
+ */
+public class MaximumProductSubarray {
+
+    /**
+     * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+     *
+     * Example 1:
+     *
+     * Input: [2,3,-2,4]
+     * Output: 6
+     * Explanation: [2,3] has the largest product 6.
+     *
+     * Example 2:
+     *
+     * Input: [-2,0,-1]
+     * Output: 0
+     * Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+     */
+
+    /**
+     * 乘积最大子序列: 给顶一个整型数字数组(至少有一个元素),找到这个数组中最大乘积顶连续子数组(即连续的两个元素乘积最大)
+     *
+     * 思路一: 循环找出所有子序列的乘积,比较大小
+     * 思路二: 采用动态规划思想进行递推
+     */
+
+    public int maxProduct(int[] nums) {
+
+        return 0;
+    }
+}
