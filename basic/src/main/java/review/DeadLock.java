529bf03c71982d01f03a22d401ee2bd3e8477d87
xiaoxiaoxiang <xiaoxiaoxiang@xhqb.com>
2021-02-03 18:12:14 +0800

    add basic demo

---
+++ basic/src/main/java/review/DeadLock.java
@@ -0,0 +1,51 @@
+package review;
+
+/**
+ * @description:
+ * @author: xiaoxiaoxiang
+ * @date: 2021/2/3 17:54
+ */
+public class DeadLock implements Runnable {
+    public int flag;
+    private Object o1 = new Object();
+    private Object o2 = new Object();
+
+    @Override
+    public void run() {
+        System.out.println("线程: " + Thread.currentThread().getName() + "的flag = " + flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o2) {
+                    System.out.println("flag == 1 的线程获得了两把锁");
+                }
+            }
+        } else {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o1) {
+                    System.out.println("flag != 1 的线程获得了两把锁");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DeadLock deadLock1 = new DeadLock();
+        DeadLock deadLock2 = new DeadLock();
+        deadLock1.flag = 1;
+        deadLock2.flag = 2;
+        new Thread(deadLock1, "t1").start();
+        new Thread(deadLock2, "t2").start();
+
+
+    }
+}
--- basic/src/main/java/review/juc/list/CopyOnWriteArrayListDemo.java
+++ basic/src/main/java/review/juc/list/CopyOnWriteArrayListDemo.java
@@ -1,9 +1,24 @@
 package review.juc.list;
 
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * @description:
  * @author: xiaoxiaoxiang
  * @date: 2021/2/3 14:25
  */
 public class CopyOnWriteArrayListDemo {
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>(new Integer[]{1, 2, 3});
+        Iterator<Integer> iterator = list.iterator();
+        list.add(4);
+        Iterator<Integer> iterator2 = list.iterator();
+
+        iterator.forEachRemaining(System.out::print);
+        System.out.println();
+        iterator2.forEachRemaining(System.out::print);
+        System.out.println();
+    }
 }
