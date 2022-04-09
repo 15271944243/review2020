5bd0d129b9ab22a6f7cb01a739f69a997594c206
xxx <190899887@qq.com>
2022-04-09 11:06:38 +0800

    add druid

---
+++ springboot/src/main/java/review/Application.java
@@ -0,0 +1,17 @@
+package review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: xiaoxiaoxiang
+ * @date: 2022/4/9 11:02
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
